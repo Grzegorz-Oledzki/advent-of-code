cd5407a6daf4c4891459d7dafeab9e8fa3db0cc3
grzegorz-oledzki <you@example.com>
2021-05-27 21:47:21 +0200

    add def get_input_coords

--- day6_2018.py
+++ day6_2018.py
@@ -1,3 +1,20 @@
-with open ("input6.txt", "r") as file:
+import numpy as np
+
+with open ("test_input.txt", "r") as file:
     inputs = file.read().split("\n")
-print(inputs)
+inputs = [x.split(", ") for x in inputs]
+
+def get_input_coords(input):
+    first_coord, second_coord = [], []
+    for coord in inputs:
+        first_coord.append(coord[0])
+        second_coord.append(coord[1])
+    return first_coord, second_coord
+
+if __name__ == "__main__":
+
+    area = np.zeros((12, 12))
+    area[1, 1] += 1
+    #print(area)
+    a,b = get_input_coords(inputs)
+    print(a,b)
