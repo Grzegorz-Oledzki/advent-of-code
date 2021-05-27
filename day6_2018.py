1e74240242693f5e3fe39b16f47fdd1f4269979c
grzegorz-oledzki <you@example.com>
2021-05-27 21:52:20 +0200

    add def get_input_coords

--- day6_2018.py
+++ day6_2018.py
@@ -7,14 +7,16 @@ inputs = [x.split(", ") for x in inputs]
 def get_input_coords(input):
     first_coord, second_coord = [], []
     for coord in inputs:
-        first_coord.append(coord[0])
-        second_coord.append(coord[1])
+        first_coord.append(int(coord[0]))
+        second_coord.append(int(coord[1]))
     return first_coord, second_coord
 
 if __name__ == "__main__":
 
-    area = np.zeros((12, 12))
+    area = np.zeros((10, 10))
+    a, b = get_input_coords(inputs)
     area[1, 1] += 1
-    #print(area)
-    a,b = get_input_coords(inputs)
-    print(a,b)
+    for first_coord, second_coord in zip(a,b):
+        area[first_coord:second_coord] += 1
+    print(area)
+
