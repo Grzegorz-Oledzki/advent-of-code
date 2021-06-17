b4e290c9e4d89bf8f9af53fba5644b65ca894428
grzegorz-oledzki <you@example.com>
2021-06-17 19:31:37 +0200

    add get_number_of_closest_points, get_all_numbers_of_closest_points, get_finite_coords functions

--- day6_2018.py
+++ day6_2018.py
@@ -2,6 +2,7 @@ with open ("test_input.txt", "r") as file:
     inputs = file.read().split("\n")
 inputs = [x.split(", ") for x in inputs]
 
+
 def get_input_coords(input):
     first_coord, second_coord = [], []
     for coord in inputs:
@@ -9,55 +10,75 @@ def get_input_coords(input):
         first_coord.append(int(coord[1]))
     return first_coord, second_coord
 
+
 def manhattan_distance(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
 
-def coords_limits(x_coord, y_coord):
-    min_x, max_x = min(x_coord), max(x_coord)
-    min_y, max_y = min(y_coord), max(y_coord)
+
+def coords_limits(x_coords, y_coords):
+    min_x, max_x = min(x_coords), max(x_coords)
+    min_y, max_y = min(y_coords), max(y_coords)
     return min_x, max_x, min_y, max_y
 
-def ignore_infinite_coords(x_coord, y_coord):
-    min_x, max_x, min_y, max_y = coords_limits(x_coord, y_coord)
-    a = False
-    if x_coord == min_x or x_coord == max_x or y_coord == min_y or y_coord == max_y:
-        a = True
-    return a
 
 def get_distance_to_all_points(x_coords, y_coords):
     all_distances = []
-    #min_x, max_x, min_y, max_y = coords_limits(x_coords, y_coords)
     coordinates = zip(x_coords, y_coords)
     for x_coord, y_coord in coordinates:
         distance = []
-        # if x_coord == min_x or x_coord == max_x or y_coord == min_y or y_coord == max_y:
-        #     continue
-        for row_idx in range(10):
-            for column_idx in range(10):
+        for row_idx in range(max(x_coords)):
+            for column_idx in range(max(y_coords)):
                 distance.append(manhattan_distance(x_coord, y_coord, row_idx, column_idx))
         all_distances.append(distance)
     return all_distances
 
+
+def get_number_of_closest_points(all_distances,distances):
+    closest_points = 0
+    for distance_idx, distance in enumerate(distances):
+        count = 0
+        for all_distances_index in range(len(all_distances)):
+            if distance < all_distances[all_distances_index][distance_idx]:
+                count += 1
+            if count == len(all_distances)-1:
+                closest_points += 1
+    return closest_points
+
+
+def get_all_numbers_of_closest_points(all_distances):
+    numbers_of_closest_points = []
+    for distances_index in range(len(all_distances)):
+        numbers_of_closest_points.append(get_number_of_closest_points(all_distances,all_distances[distances_index]))
+    return numbers_of_closest_points
+
+
+def get_finite_coords(coords):
+    min_x, max_x, min_y, max_y = coords_limits(x_coords, y_coords)
+    finite_coords = []
+    for x_coord, y_coord in coords:
+        if x_coord == min_x or x_coord == max_x or y_coord == min_y or y_coord == max_y:
+            continue
+        else:
+            finite_coords.append((x_coord, y_coord))
+    return finite_coords
+
+#def find_numbers_of_closest_points_finite_coords(finite_coords):
+
+
+
+
 if __name__ == "__main__":
-
-
 
     x_coords, y_coords = get_input_coords(inputs)
     all_distances = get_distance_to_all_points(x_coords, y_coords)
-    for distances in all_distances:
-        closest_locations = 0
-        for distance_idx, distance in enumerate(distances):
-            count = 0
-            for all_distances_index in range(1, len(all_distances)):
-                #print(distance, all_distances[all_distances_index][distance_idx])
-                if distance < all_distances[all_distances_index][distance_idx]:
-                    count += 1
-                if count == 5:
-                    closest_locations += 1
-        print(closest_locations)
+    all_numbers_of_closest_points = get_all_numbers_of_closest_points(all_distances)
+    coords = zip(x_coords, y_coords)
 
-        ##zrobic z tego funkcje ze daje sie jedne dystanse i porówna z wszystkimi innymi,
-        # a jak bedzie 0 najbliższych pól to out
+    print(all_numbers_of_closest_points)
+
+
+
+
 
 
 
